6a06c47cb9004e44c187aee984aa1a9ecf1e2f2a
Arthur Prado De Fazio <Arthur_Prado_De_Fazio@MacBook-Pro-Arthur-Prado-De-Fazio.local>
2022-12-21 20:40:48 -0300

    Implementa versao da linha de comando

--- CidadesDistancias/CidadesDistancias/Program.cs
+++ CidadesDistancias/CidadesDistancias/Program.cs
@@ -3,7 +3,60 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Entre com o número de cidades: ");
+        int numeroDeCidades = ColetaInt();
+
+        int[,] distancias = new int[numeroDeCidades, numeroDeCidades];
+        PreencheDistancias(distancias);
+
+        int distanciaPercorrida = CalculaDistanciaDoCaminho(distancias);
+        Console.WriteLine($"A distância percorrida foi {distanciaPercorrida}");
+    }
+
+    private static int CalculaDistanciaDoCaminho(int[,] distancias)
+    {
+        Console.WriteLine("Entre com o número de cidades em que o caminho vai parar, incluindo as cidades de partida e chegada");
+        int cidadesParadas = ColetaInt();
+
+        int distanciaPercorrida = 0;
+        Console.WriteLine("Entre com a cidade de partida");
+        int anterior, atual;
+        anterior = ColetaInt() - 1;
+        for (int i = 1; i < cidadesParadas; i++)
+        {
+            Console.WriteLine("Entre com a próxima cidade");
+            atual = ColetaInt() - 1;
+
+            if (atual < anterior)
+                distanciaPercorrida += distancias[anterior, atual];
+            else
+                distanciaPercorrida += distancias[atual, anterior];
+
+            anterior = atual;
+        }
+
+        return distanciaPercorrida;
+    }
+
+    public static int ColetaInt()
+    {
+        int i;
+        Console.Write("Entre com um número inteiro maior ou igual a zero: ");
+        while (!int.TryParse(Console.ReadLine(), out i) || i < 0)
+            Console.WriteLine("Entrada inválida, entre com um poisitvo maior ou igual a zero: ");
+        return i;
+    }
+
+    public static void PreencheDistancias(int[,] distancias)
+    {
+        for (int i = 0; i < distancias.GetLength(0); i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                Console.WriteLine($"Entre com a distância entre as cidades {i + 1} e {j + 1}");
+                distancias[i, j] = ColetaInt();
+            }
+        }
     }
 }
 
